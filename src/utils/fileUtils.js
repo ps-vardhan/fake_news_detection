10a13ab05e14d29e9539cb285535c3f8cc0d253e
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-01-28 13:07:14 +0530

    Pre-cleanup

--- src/utils/fileUtils.js
+++
@@ -1,9 +0,0 @@
-import { existsSync, mkdirSync } from 'fs';
-
-export const createDirectory = (dirPath) => {
-  if (!existsSync(dirPath)) {
-    mkdirSync(dirPath, { recursive: true });
-    return true;
-  }
-  return false;
-};
